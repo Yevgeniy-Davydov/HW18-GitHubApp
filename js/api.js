00dde89407cdef10abc12580c75e2061713cfade
Yevgeniy-Davydov <131383721+Yevgeniy-Davydov@users.noreply.github.com>
2024-04-26 23:12:02 +0200

    new api token added

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_TOKEN = "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";
+const API_TOKEN = "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";
 const API_URL = "https://api.github.com";
 
 export const getUser = async (userName) => {
